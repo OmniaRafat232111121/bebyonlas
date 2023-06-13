a9b6460254d5149bec1860ef7ed422b19f418477
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2023-06-14 00:39:56 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    newNextLinkBehavior: false,
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+    return config;
+  },
+};
+
+module.exports = nextConfig;
